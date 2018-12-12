b88d65de75f6731de5ba9375284e232f4f5526d3
John Raub <raublekick@gmail.com>
2018-12-12 16:06:31 -0700

    Initial comit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" :data="data"/>
   </div>
 </template>
 
@@ -10,6 +10,14 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      data: {
+        property1: "Property 1",
+        property2: "Property 2"
+      }
+    };
+  },
   components: {
     HelloWorld
   }
